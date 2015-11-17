80f33d4236bf966b0f060fca26de87afe3026b66
Christian Blanquera <cblanquera@openovate.com>
2015-11-17 13:12:05 +0800

    test requirements update for travis

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,10 +9,20 @@
 
 namespace 
 {
+	if(file_exists(__DIR__.'/../../../autoload.php')) {
+		require_once __DIR__.'/../../../autoload.php';
 	
-	require_once __DIR__.'/../../../autoload.php';
-	
-	Eden::DECORATOR;
+		Eden::DECORATOR;
+	} else {
+		require_once __DIR__.'/../src/Argument.php';
+		require_once __DIR__.'/../src/Base.php';
+		require_once __DIR__.'/../src/Exception.php';
+		require_once __DIR__.'/../src/Route.php';
+		require_once __DIR__.'/../src/Control.php';
+		require_once __DIR__.'/../src/Event.php';
+		require_once __DIR__.'/../src/Index.php';
+		require_once __DIR__.'/../src/Inspect.php';
+	}
 	
  	class Bar_Foo_Zoo extends Eden\Core\Base
     {
